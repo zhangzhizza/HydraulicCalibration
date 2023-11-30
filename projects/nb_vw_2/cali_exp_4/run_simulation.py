2a94fec1b7733de36cd6f9ed4261f61e3464bb7d
AI-BC3 <zhiang.zhang@nottingham.edu.cn>
2023-11-30 17:08:21 +0800

    datadriven notebook added

--- projects/nb_vw_2/cali_exp_4/run_simulation.py
+++ projects/nb_vw_2/cali_exp_4/run_simulation.py
@@ -57,9 +57,10 @@ sim_inputs_test = {'pump_speed_1.k': input_test_data['NB2_S_1_NYZ_cwp_9_HzSPR_x'
 outputs = ['chw_sup_m.m_flow', 'chw_sup_P.k', 'terminal_resist.y_actual']
 time_s = time.time()
 sim_res_cali = simulator.simulate(inputs=sim_inputs_cali, output_names=outputs, threads_n = 1, check_pressure = False)
-sim_res_test = simulator.simulate(inputs=sim_inputs_test, output_names=outputs, threads_n = 1, check_pressure = False)
-print('Running time: {}s'.format(time.time() - time_s))
 np.savetxt(res_dir + os.sep + 'sim_res_cali.csv', 
             sim_res_cali, delimiter = ',')
+sim_res_test = simulator.simulate(inputs=sim_inputs_test, output_names=outputs, threads_n = 1, check_pressure = False)
 np.savetxt(res_dir + os.sep + 'sim_res_test.csv', 
             sim_res_test, delimiter = ',')
+print('Running time: {}s'.format(time.time() - time_s))
+
